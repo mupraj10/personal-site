ef4803dd86192de30b5736869c0588572fdf6116
Maggy Prajapati <mupraj10@gmail.com>
2017-12-11 23:23:19 -0500

    added paralax

--- app/components/Root.jsx
+++ app/components/Root.jsx
@@ -1,41 +1,68 @@
-import React, { Component } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import React, { Component } from "react";
+import Parallax from 'react-springy-parallax'
 
-//components being imported in
-import Home from './Home';
-import AboutMe from './AboutMe'
-import Projects from './Projects';
-import Skills from './Skills';
-import ContactMe from './ContactMe';
-import Resume from './Resume'
-
-import ClickMenu from './ClickMenu'
-import Navbar from './Navbar';
-import Footer from './Footer';
 
 export default class Root extends Component {
   constructor() {
-    super()
+    super();
   }
 
   render() {
+    const styles = {
+      fontFamily: "Menlo-Regular, Menlo, monospace",
+      fontSize: 14,
+      lineHeight: "10px",
+      color: "white",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    };
+
     return (
-      <Router>
-        
-        <div  className="vh-100 dt w-100 bg-light-gray avenir next">
-        <Navbar />
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/aboutme' component={AboutMe} />
-            <Route exact path='/projects' component={Projects} />
-            <Route exact path='/skills' component={Skills} />
-            <Route exact path='/resume' component={Resume} />
-            <Route exact path='/contactme' component={ContactMe} />
-          </Switch>
-          {/* <Footer /> */}
-        </div>
-      </Router>
-    )
+      <Parallax ref="parallax"  pages={3}>
+        <Parallax.Layer
+          offset={0}
+          speed={1}
+          style={{ backgroundColor: "#243B4A" }}
+        />
+        <Parallax.Layer
+          offset={1}
+          speed={1}
+          style={{ backgroundColor: "#805E73" }}
+        />
+        <Parallax.Layer
+          offset={2}
+          speed={1}
+          style={{ backgroundColor: "#87BCDE" }}
+        />
+
+        <Parallax.Layer
+          offset={0}
+          speed={0}
+          style={styles}
+          onClick={() => this.refs.parallax.scrollTo(1)}
+        >
+          Click!
+        </Parallax.Layer>
+
+        <Parallax.Layer
+          offset={1}
+          speed={-0.1}
+          style={styles}
+          onClick={() => this.refs.parallax.scrollTo(2)}
+        >
+          Home !!!
+        </Parallax.Layer>
+
+        <Parallax.Layer
+          offset={2}
+          speed={0.5}
+          style={styles}
+          onClick={() => this.refs.parallax.scrollTo(0)}
+        >
+          The end.
+        </Parallax.Layer>
+      </Parallax>
+    );
   }
 }
-
